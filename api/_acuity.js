aa7256982ff90b5e05896ff91acadaed1bf59b10
austendvds-boop <austen.dvds@gmail.com>
2025-10-28 13:02:20 -0700

    Fix Vercel config schema and serve root UI

--- api/_acuity.js
+++ api/_acuity.js
@@ -1,13 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
-let _types, _loc;
+let _types, _locCfg;
 
-function readJSON(p){
+function readJSON(p) {
   const abs = path.resolve(process.cwd(), p);
   return JSON.parse(fs.readFileSync(abs, 'utf8'));
 }
-function loadCityTypes(){ return _types || (_types = readJSON('city-types.json')); }
-function loadLocationConfig(){ return _loc || (_loc = readJSON('location-config.json')); }
 
-module.exports = { loadCityTypes, loadLocationConfig };
+function loadCityTypes() {
+  return _types || (_types = readJSON('city-types.json'));
+}
+
+function loadLocationConfig() {
+  return _locCfg || (_locCfg = readJSON('location-config.json'));
+}
+
+module.exports = {
+  loadCityTypes,
+  loadLocationConfig,
+};
