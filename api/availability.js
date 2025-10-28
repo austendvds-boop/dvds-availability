6fbd2c55e6d66fd93546c0e72a3db5948651f9ca
austendvds-boop <austen.dvds@gmail.com>
2025-10-28 12:16:21 -0700

    Add Acuity scheduling metadata for check city locations

--- api/availability.js
+++ api/availability.js
@@ -1,3 +1,7 @@
+const path = require("path");
+
+const checkCityData = require(path.join(__dirname, "check_city.json"));
+
 module.exports = (req, res) => {
-  res.status(200).json({ ok: true, path: "/api/availability" });
+  res.status(200).json({ ok: true, locations: checkCityData });
 };
